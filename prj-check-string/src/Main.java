06517d76f4181e60526ae5754deea142b8376d48
Nguyễn Xuân Tuân <tuandzpc2000@gmail.com>
2022-10-18 08:46:59 +0700

    commit

--- prj-check-string/src/Main.java
+++ prj-check-string/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+
 public class Main {
+
+    public static Boolean isPangram (String s) {
+        if(s.length() <26) {
+            return false;
+        } else {
+            for(char ch = 'a' ; ch <= 'z' ;ch++){
+                if (s.indexOf(ch)<0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Kiểm tra chuỗi đã cho có phải pangram ( từ a -> z)
+        String s = "1234444444444444444444444444444444444444444";
+
+        System.out.println(isPangram(s.toLowerCase()));
+
     }
+
+
 }
